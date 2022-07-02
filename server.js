78a15359fbec11407fc66850ca82ee0647f5574f
Kai Nicholas <mail@kainicholas.co.uk>
2022-07-02 22:17:46 +0100

    allow https

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const PORT = process.env.PORT
 const io = require('socket.io')(PORT, {
     cors: {
-        origin: ['http://kn215.brighton.domains'],
+        origin: ['http://kn215.brighton.domains', 'https://kn215.brighton.domains'],
     },
 })
 
